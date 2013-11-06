e1964d38f33f80aaf653e9710ed372847e023cf1
Florian Grundig <florian.grundig@gmx.de>
2013-11-06 16:46:28 +0100

    more tests

---
+++ karma_test_project/unit-tests/lotsOfTestsSpec.js
@@ -0,0 +1,230 @@
+
+
+describe('PhoneListCtrl', function(){
+
+
+  it('should count 1', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 2', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 3', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 4', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 5', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 6', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 7', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 8', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 9', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 10', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 11', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 12', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 13', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 14', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 15', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 16', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 17', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 18', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 19', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 20', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 21', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 22', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 23', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 24', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 25', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 26', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 27', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 28', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 29', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 30', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 31', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 32', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 33', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 34', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 35', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 36', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 37', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 38', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 39', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 40', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 41', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 42', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 43', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 44', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 45', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 46', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 47', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 48', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 49', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 50', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 51', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 52', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 53', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 54', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 55', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 56', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 57', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 58', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 59', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 60', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 61', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 62', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 63', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 64', function() {
+    expect([]).toEqual([]);
+  });
+
+  it('should count 65', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 66', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 67', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 68', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 69', function() {
+    expect([]).toEqual([]);
+  });
+  it('should count 70', function() {
+    expect([]).toEqual([]);
+  });
+});
